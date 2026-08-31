de45b70caf3d8e0683b6dadb3600765c9640a32c
3spada <r3dlavaofficial@gmail.com>
2026-08-31 16:47:04 +0700

    Ganti better-sqlite3 dengan sqlite3 standar

--- db.js
+++ db.js
@@ -1,24 +1,24 @@
 const path = require('path');
 const fs = require('fs');
-const Database = require('better-sqlite3');
+const sqlite3 = require('sqlite3').verbose();
 
-// Gunakan direktori data sementara atau pastikan jalur absolut
 const dbDir = process.env.RAILWAY_VOLUME_MOUNT_PATH || __dirname;
 const DB_PATH = path.join(dbDir, '3spada.db');
 
-const db = new Database(DB_PATH);
-
-try {
-  const schemaPath = path.join(__dirname, 'schema.sql');
-  if (fs.existsSync(schemaPath)) {
-    const schema = fs.readFileSync(schemaPath, 'utf8');
-    db.exec(schema);
-    console.log('Database dan skema berhasil dimuat.');
+const db = new sqlite3.Database(DB_PATH, (err) => {
+  if (err) {
+    console.error('Gagal membuka database:', err.message);
   } else {
-    console.error('File schema.sql tidak ditemukan di:', schemaPath);
+    console.log('Terhubung ke database SQLite.');
+    const schemaPath = path.join(__dirname, 'schema.sql');
+    if (fs.existsSync(schemaPath)) {
+      const schema = fs.readFileSync(schemaPath, 'utf8');
+      db.exec(schema, (err) => {
+        if (err) console.error('Gagal menjalankan skema:', err);
+        else console.log('Skema database berhasil dimuat.');
+      });
+    }
   }
-} catch (err) {
-  console.error('Gagal menjalankan schema.sql:', err);
-}
+});
 
 module.exports = db;
